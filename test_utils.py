c7bb27066da8d565b22ba5a64a8ef797db0d9a29
milan404 <milan404@hotmail.com>
2024-04-07 15:54:26 +0200

    Added some .py files

---
+++ test_utils.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 14:42:11 2024
+
+@author: milan
+"""
+
+from utils import funny
+
+def test_funny():
+    assert funny(2, 1) == 3
+    
+    
---
+++ utils.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  7 14:39:03 2024
+
+@author: milan
+"""
+
+def funny(x, y):
+    return x + y
