f0d305d285ce5da0759c14625af4d58e3d34ec14
milan404 <milan404@hotmail.com>
2024-04-08 23:35:19 +0200

    ruff formatted

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 """
 Calculation of investment and replacement costs for an energy storage system
 """
+
 import utils
 
-#specific ess power capacity investment cost [EUR/kW]
+# specific ess power capacity investment cost [EUR/kW]
 cp_inv = 500
-#specific ess energy capacity investment cost [EUR/kWh]
+# specific ess energy capacity investment cost [EUR/kWh]
 ce_inv = 500
-#specific ess power capacity replacemen cost [EUR/kW]
+# specific ess power capacity replacemen cost [EUR/kW]
 cp_rep = 100
-#specific ess energy capacity replacement cost [EUR/kWh]
+# specific ess energy capacity replacement cost [EUR/kWh]
 ce_rep = 200
-#nominal ess power capacity kW
+# nominal ess power capacity kW
 cap_p_nom = 1
-#nominal ess energy capacity kWh
+# nominal ess energy capacity kWh
 cap_e_nom = 1000
 
 
-#calculating investment costs of an ess
+# calculating investment costs of an ess
 investment_costs = utils.investment_costs(cp_inv, cap_p_nom, ce_inv, cap_e_nom)
 
-#calculating replacement costs of an ess
+# calculating replacement costs of an ess
 replacement_costs = utils.replacement_costs(cp_rep, cap_p_nom, ce_rep, cap_e_nom)
 
 print("\nInvestment costs are " + str(investment_costs) + " EUR")
--- utils.py
+++ utils.py
@@ -4,11 +4,12 @@ and replacement costs for energy storage systems.
 The parameters are defined in main.py.
 """
 
+
 def investment_costs(cp_inv, cap_p_nom, ce_inv, cap_e_nom):
     """Calculation of investment costs for energy storage system"""
     return cp_inv * cap_p_nom + ce_inv * cap_e_nom
 
+
 def replacement_costs(cp_rep, cap_p_nom, ce_rep, cap_e_nom):
     """Calculation of replacement costs for energy storage system"""
     return cp_rep * cap_p_nom + ce_rep * cap_e_nom
-
