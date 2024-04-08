cc298d7377d57eb39b467c143694932ac26d4e4a
milan404 <milan404@hotmail.com>
2024-04-08 21:47:42 +0200

    Added functions to utils

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sun Apr  7 14:39:03 2024
-
-@author: milan
+This module defines the functions required for calculating investment costs
+and replacement costs for energy storage systems.
+The parameters are defined in main.py
 """
 
-def funny(x, y):
-    return x + y
+def investments_costs(cp_inv, cap_p_nom, ce_inv, cap_e_nom,):
+    """Calculation of investment costs for energy storage system"""
+    return cp_inv * cap_p_nom + ce_inv * cap_e_nom
+
+def replacements_costs(cp_rep, cap_p_nom, ce_rep, cap_e_nom):
+    """Calculation of replacement costs for energy storage system"""
+    return cp_rep * cap_p_nom + ce_rep * cap_e_nom
+
